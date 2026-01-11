593794af4f565921cabe35ffd1a78428fcc0f273
Vanessa11122 <vanessa.mueller@mng.ch>
2026-01-11 17:24:27 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -156,15 +156,6 @@ def add_trip():
 
     return render_template('add_trip.html')
 
-@app.route('/add_trip/<int:country_id>')
-@login_required
-def add_trip(country_id):
-    db_write(
-        "INSERT INTO user_trips (user_id, country_id) VALUES (%s, %s)",
-        (current_user.id, country_id)
-    )
-    return redirect(url_for('index'))
-
 
 
    
