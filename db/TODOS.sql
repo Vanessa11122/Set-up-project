9eaf2664fcf7ec0d09f831cf9457bd2191f123db
Vanessa11122 <vanessa.mueller@mng.ch>
2026-01-11 17:12:18 +0100

    Update TODOS.sql
    user.id bei Table users hinzugefügt

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(250) NOT NULL UNIQUE,
     password VARCHAR(250) NOT NULL
+    user2.id INT NOT NULL,
 );
 
 CREATE TABLE todos (
