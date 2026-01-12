c0252013b74ed5fb41dcdb45a5ccfa97690ff78b
Vanessa11122 <vanessa.mueller@mng.ch>
2026-01-12 23:22:07 +0100

    Update flask_app.py
    country_id

--- flask_app.py
+++ flask_app.py
@@ -145,12 +145,11 @@ def add_trip():
         reiseziel = db_read("SELECT country_id, country FROM destinations ORDER BY country",(current_user2.id,))
         return render_template("add_trip.html", reiseziel=reiseziel)
         
-    country_id = request.form["country_id"]
+    country = request.form.get('country_id') 
     db_write("INSERT INTO destinations (country_id, country) VALUES (%s, %s)", (current_user2.id, country_id))
     return redirect(url_for('index'))
 
     return render_template('add_trip.html')
-
 
 
 
