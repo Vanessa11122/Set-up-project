dad70e18e9216e1d45c8762ab4a094504296b4fa
Vanessa11122 <vanessa.mueller@mng.ch>
2026-01-14 20:47:35 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -150,10 +150,14 @@ def Italien():
 def frankreich():
     if request.method == "POST":
         reiseziel = request.form["reiseziel"]
-
+        hotels = request.form["hotels"]
         db_write(
             "INSERT INTO reiseziele (name) VALUES (%s)",
             (reiseziel,)
+        )
+        db_write(
+            "INSERT INTO hotels (name) VALUES (%s)",
+            (hotels,)
         )
 
         # WICHTIG: Redirect nach POST
@@ -161,8 +165,9 @@ def frankreich():
 
     # GET: Daten aus DB holen
     reiseziele = db_read("SELECT name FROM reiseziele WHERE land LIKE '%Frankreich%'")
-
-    return render_template("Frankreich.html", reiseziele=reiseziele)
+    hotels = db_read("SELECT name FROM hotels WHERE land LIKE '%Frankreich%'")
+    
+    return render_template("Frankreich.html", reiseziele=reiseziele, hotels=hotels)
 
 
     
