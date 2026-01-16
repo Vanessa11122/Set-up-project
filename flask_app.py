96140fa9dfc24686809a64884948efa106bc3ef0
Vanessa11122 <vanessa.mueller@mng.ch>
2026-01-16 17:23:49 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, abort
 from dotenv import load_dotenv
 import os
 import git
